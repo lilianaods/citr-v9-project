0f0cf05ef3b764a6c4c85f81418234caa8bfb197
Liliana Silva <lilianaods@outlook.com>
2026-01-17 19:22:57 -0400

    add static assets

--- padre-ginos/src/Pizza.jsx
+++ padre-ginos/src/Pizza.jsx
@@ -1,10 +1,11 @@
 function Pizza(props) {
   return (
     <div className="pizza">
-        <h1>{props.name}</h1>
-        <p>{props.description}</p>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
     </div>
-  )
+  );
 }
 
 export default Pizza;
