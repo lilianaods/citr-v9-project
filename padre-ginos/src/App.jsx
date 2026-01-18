e4c4678fdf12e83a1dac8e0960bc957e2df0ab1d
Liliana Silva <lilianaods@outlook.com>
2026-01-18 18:08:32 -0400

    load and show pizza of the day

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 function App() {
   return (
     <div>
       <h1>Padre Gino's Pizza – Order Now</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 }
